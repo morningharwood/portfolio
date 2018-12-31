1d978835653e99020792ce6bd9c6ca704318962c
Matthew Harwood <harwood@uber.com>
2018-12-31 09:12:46 -0800

    Added global styletron decorator.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,17 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import {Provider} from 'styletron-react';
+import {Client as Styletron} from "styletron-engine-atomic";
 
+const engine = new Styletron();
 const req = require.context('../libs', true, /\.story\.js$/);
-console.log(req);
+
+addDecorator(story => (
+  <Provider value={engine}>
+    {story()}
+  </Provider>
+))
+
 function loadStories() {
   req.keys().forEach(req);
 }
--- libs/mh-header/src/index.story.js
+++ libs/mh-header/src/index.story.js
@@ -1,20 +1,12 @@
 // @flow
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import {Provider, styled} from "styletron-react";
-import {Client as Styletron} from "styletron-engine-atomic";
 
 import Header from './index';
 
-const engine = new Styletron();
 
 
 storiesOf('Header', module)
-  .addDecorator(story => (
-    <Provider value={engine}>
-      {story()}
-    </Provider>
-  ))
   .add('with default fixtures', () => (
     <Header
       title={'Nike Jordan Editor'}
