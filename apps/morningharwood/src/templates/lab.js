fd5df477a8c43815e55abff553a30fb84aa45f4b
Matthew Harwood <harwood@uber.com>
2019-01-10 20:17:30 -0800

    removed dist.

--- apps/morningharwood/src/templates/lab.js
+++ apps/morningharwood/src/templates/lab.js
@@ -5,8 +5,6 @@ const Template = ({pageContext: ctx}) => {
   return (
     <div>
       <h1>{JSON.stringify(ctx.data)}</h1>
-      <MhHeader data={ctx.data.header} />
-      <Navgation data={ctx.data.navigation} />
     </div>
   );
 };
